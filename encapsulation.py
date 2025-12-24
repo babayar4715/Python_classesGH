21c24bdf52ff5b80f903eb2bea4e255222742c71
Babaiar <119310971+Babaiaro@users.noreply.github.com>
2025-12-23 23:57:54 -0600

    Add files via upload

---
+++ encapsulation.py
@@ -0,0 +1,13 @@
+class Student:
+    def __init__(self, name="Rajaram", marks=50):
+        self.name
+        self.marks
+
+s1.Student()
+s2.Student("Bharat", 25)
+
+
+s1.studentdata()
+s2.studentdata()
+print("Name: {} marks: {}". format(s1.__name, s2.__marks))
+print("Name: {} marks: {}". format(s2.__name, s2.__makrs))
